19645af7fe380a2eb84395ec4ebb1d1108fcdf10
jp <jp@big-camel>
2016-05-24 18:49:21 +0200

    added: append message to already existing testcase in a testsuite

--- src/jUnitXmlReporter/TestSuite.java
+++ src/jUnitXmlReporter/TestSuite.java
@@ -1,31 +1,41 @@
 package jUnitXmlReporter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class TestSuite
 {
 	private String name;
-	private ArrayList<TestCase> testCaseList;
+	private HashMap<String, TestCase> testCaseMap;
 	
 	public TestSuite(String name)
 	{
 		this.setName(name);
-		this.testCaseList = new ArrayList<>();
+		this.testCaseMap = new HashMap<>();
 	}
 	
 	public void addTestCase(TestCase tc)
 	{
-		testCaseList.add(tc);
+		if(containsTestCase(tc))
+		{
+			String oldMessage = testCaseMap.get(tc.getName()).getMessage();
+			String newMessage = oldMessage + "\n" + tc.getMessage();
+			testCaseMap.get(tc.getName()).setMessage(newMessage);
+		}
+		else
+			testCaseMap.put(tc.getName(), tc);
 	}
 
-	public ArrayList<TestCase> getTestCaseList()
+	private boolean containsTestCase(TestCase tc)
 	{
-		return testCaseList;
+		return testCaseMap.containsKey(tc.getName()) &&
+				testCaseMap.get(tc.getName()).getType().equals(tc.getType());
 	}
-
-	public void setTestCaseList(ArrayList<TestCase> testCaseList)
+	
+	public List<TestCase> getTestCaseList()
 	{
-		this.testCaseList = testCaseList;
+		return new ArrayList<TestCase>(testCaseMap.values());
 	}
 
 	public String getName()
